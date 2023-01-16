58f4368d1251e32bae593f052246123e4129418f
evgeniyamiller <evgeniya_hvorostova@mail.ru>
2023-01-17 00:48:56 +0400

    Initial commit

---
+++ src/main/java/com/skypro/arraylist/ArrayList3Application.java
@@ -0,0 +1,13 @@
+package com.skypro.arraylist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArrayList3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArrayList3Application.class, args);
+	}
+
+}
